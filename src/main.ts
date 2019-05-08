bfce943a5655c9d0a3cfd84d6d64c2334d6c2358
Ken Mueller <kenmueller0@gmail.com>
2019-05-07 23:30:10 -0700

    optimized

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 let window: Electron.BrowserWindow | null
 
 function createWindow() {
-	window = new BrowserWindow({ height: 600, width: 800 })
+	window = new BrowserWindow({ title: 'Astra Exchange', height: 800, width: 1300 })
 	window.loadFile(path.join(__dirname, '../index.html'))
 	window.on('closed', () => window = null)
 }
